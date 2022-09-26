3fa0dc7ff3bd604899dd670f826563d3b1c8c381
walacenascimento <wlcens@hotmail.com>
2022-09-26 20:17:10 -0300

    Resolvendo o requisito 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,18 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "nome_do_produto",
+        "nome_da_empresa",
+        "26/09/2022",
+        "26/09/2025",
+        "numero_de_serie",
+        "intrucoes_de_armazenamento",
+    )
+    assert product.id == 1
+    assert product.nome_do_produto == "nome_do_produto"
+    assert product.data_de_fabricacao == "26/09/2022"
+    assert product.data_de_validade == "26/09/2025"
+    assert product.instrucoes_de_armazenamento == "intrucoes_de_armazenamento"
