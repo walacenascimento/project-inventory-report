e4f3ef30ad1260389ae59ce25d8e2c82f054d551
walacenascimento <wlcens@hotmail.com>
2022-09-26 20:47:51 -0300

    Resolve o requisito 2

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "nome_do_produto",
+        "nome_da_empresa",
+        "26/09/2022",
+        "26/09/2025",
+        "numero_de_serie",
+        "intrucoes_de_armazenamento",
+    )
+    assert str(product) == (
+        f"O produto {product.nome_do_produto} "
+        f"fabricado em {product.data_de_fabricacao} "
+        f"por {product.nome_da_empresa} "
+        f"com validade até {product.data_de_validade} "
+        f"precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )
