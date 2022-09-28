5b0e5d4c3f7ea92fa24bf44a790a18373c6e6c88
walacenascimento <wlcens@hotmail.com>
2022-09-27 21:03:56 -0300

    Resolve o requisito 7

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,13 +1,15 @@
 import csv
 import json
+import xml.etree.cElementTree as WN
 from ..reports.simple_report import SimpleReport
 from ..reports.complete_report import CompleteReport
 
-# Requisito 5 e 6
+# Requisito 5, 6 e 7
 
 
 class Inventory:
     @classmethod
+    # Requisito 5
     def import_data(cls, file, type_report):
         lista_produtos = cls.import_file(file)
         if type_report == "simples":
@@ -16,6 +18,7 @@ class Inventory:
             return CompleteReport.generate(lista_produtos)
 
     @classmethod
+    # Requisito 6
     def import_file(cls, file):
         lista_produtos = []
         if file.endswith("csv"):
@@ -23,8 +26,18 @@ class Inventory:
                 reader = csv.DictReader(csv_file)
                 lista_produtos = [row for row in reader]
             return lista_produtos
-        if file.endswith("json"):
+
+        elif file.endswith("json"):
             with open(file) as file_json:
                 reader = json.load(file_json)
                 lista_produtos = [row for row in reader]
                 return lista_produtos
+        # Requisto 7
+        elif file.endswith("xml"):
+            with open(file) as file_xml:
+                tree = WN.parse(file_xml)
+                root = tree.getroot()
+                lista_produtos = [
+                    {elem.tag: elem.text for elem in item} for item in root
+                ]
+            return lista_produtos
